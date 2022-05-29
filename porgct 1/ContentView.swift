2f627c4341e6f1cb2402044a60e4a84924e543fd
shoogalmalki <shoogalmalki11@gmail.com>
2022-05-29 15:05:23 +0300

    Initial Commit

---
+++ porgct 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  porgct 1
+//
+//  Created by shoog almalki on 28/10/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ porgct 1/porgct_1App.swift
@@ -0,0 +1,17 @@
+//
+//  porgct_1App.swift
+//  porgct 1
+//
+//  Created by shoog almalki on 28/10/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct porgct_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
