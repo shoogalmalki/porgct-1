4aa017257f8abbc5bb950e212bb4a6fab5705462
May <mm88dd@gmail.com>
2022-06-22 15:56:25 +0300

    new page

---
+++ porgct 1/Mmm.swift
@@ -0,0 +1,20 @@
+//
+//  Mmm.swift
+//  porgct 1
+//
+//  Created by May AlQusayer on 23/11/1443 AH.
+//
+
+import SwiftUI
+
+struct Mmm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Mmm_Previews: PreviewProvider {
+    static var previews: some View {
+        Mmm()
+    }
+}
