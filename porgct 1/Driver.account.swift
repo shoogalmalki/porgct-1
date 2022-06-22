9d5762ff588682e9df658d9145687402d38cae94
arah457 <sarahhemood558@gmail.com>
2022-06-23 02:07:24 +0300

    j

---
+++ porgct 1/Driver.account.swift
@@ -0,0 +1,20 @@
+//
+//  Driver.account.swift
+//  porgct 1
+//
+//  Created by sarah hemood  on 24/11/1443 AH.
+//
+
+import SwiftUI
+
+struct Driver_account: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Driver_account_Previews: PreviewProvider {
+    static var previews: some View {
+        Driver_account()
+    }
+}
