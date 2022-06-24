05e48651aba6183a3772f6e2cea9903bda35d85b
May <mm88dd@gmail.com>
2022-06-25 01:13:55 +0300

    driver

---
+++ porgct 1/ OrderDriver.swift
@@ -0,0 +1,20 @@
+//
+//   OrderDriver.swift
+//  porgct 1
+//
+//  Created by May AlQusayer on 26/11/1443 AH.
+//
+
+import SwiftUI
+
+struct _OrderDriver: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _OrderDriver_Previews: PreviewProvider {
+    static var previews: some View {
+        _OrderDriver()
+    }
+}
