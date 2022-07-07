bb42b15a65a5d9196964f7473404a55bf9e10bfd
arah457 <sarahhemood558@gmail.com>
2022-07-07 17:29:46 +0300

    jj

--- porgct 1/Structs.swift
+++ porgct 1/Structs.swift
@@ -317,3 +317,104 @@ struct location1 : View{
         }
     }
 }
+struct section : View {
+    var body: some View {
+        
+        
+        ZStack{
+            Rectangle()
+                .frame(width: 325, height:200)
+                .foregroundColor(.white)
+                .cornerRadius(15)
+            
+            VStack( spacing: 3){
+                Image("Image1-1")
+                    .resizable()
+                    .frame(width: 250, height:25)
+                HStack(spacing:190){
+                    Text("Taif")
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                    
+                    Text("Riyadh")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                }
+                VStack(spacing: 8){
+                    HStack(spacing:66){
+                        VStack(alignment: .leading){
+                            Text("Starting:")
+                                .font(.footnote)
+                                .fontWeight(.regular)
+                                .foregroundColor(Color(hue: 0.492, saturation: 0.851, brightness: 0.64))
+                            
+                            HStack{
+                                Image(systemName: "calendar.badge.clock")
+                                    .foregroundColor(Color(hue: 0.492, saturation: 0.851, brightness: 0.64))
+                                
+                                Text("Sun, June 5(8 -11AM)")
+                                    .font(.caption)
+                                    .foregroundColor(Color(hue: 0.492, saturation: 0.851, brightness: 0.64))
+                                
+                            }
+                            Text("Arrival:")
+                                .font(.footnote)
+                                .fontWeight(.regular)
+                                .foregroundColor(Color(hue: 0.492, saturation: 0.851, brightness: 0.64))
+                            
+                            HStack{
+                                Image(systemName: "calendar.badge.clock")
+                                    .foregroundColor(Color(hue: 0.492, saturation: 0.851, brightness: 0.64))
+                                
+                                Text("Fri, June 3(8 -11AM)")
+                                    .foregroundColor(Color(hue: 0.492, saturation: 0.851, brightness: 0.64))
+                                    .font(.caption)
+                                
+                            }
+                            
+                        }
+                        VStack{
+                            ZStack(alignment:.bottom){
+                                Image("Image2-1")
+                                    .resizable()
+                                    .frame(width: 57, height:55)
+                                
+                                ZStack(alignment: .center){
+                                    Rectangle()
+                                        .frame(width: 40, height:15)
+                                        .cornerRadius(5)
+                                    
+                                        .foregroundColor(Color("Color1"))
+                                    
+                                    HStack(alignment: .center){
+                                        Text("4.9")
+                                            .font(.caption)
+                                            .fontWeight(.medium)
+                                        
+                                        Image(systemName: "star.fill")
+                                            .foregroundColor(Color.orange)
+                                            .frame(width:5.0 , height: 10)
+                                        
+                                    }
+                                }
+                                
+                            }
+                            Text("OMER SALEH")
+                                .font(.footnote)
+                        }
+                    }
+                    Button(action: {}, label: {
+                        Text("Request")
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 18, weight: .semibold, design: .serif))
+                    })
+                    .frame(width: 300, height:35)
+                    .background(.orange)
+                    .cornerRadius(5)
+                    
+                }
+            }
+        }
+    }
+}
