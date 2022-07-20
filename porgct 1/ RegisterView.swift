17f23aaeed7a0543dde6615ed9b59f8a214d967b
lamaalj <96816554+lamaalj@users.noreply.github.com>
2022-07-20 22:29:48 +0300

    login

---
+++ porgct 1/ RegisterView.swift
@@ -0,0 +1,191 @@
+//
+//   RegisterView.swift
+//  Habhoob
+//
+//  Created by lama aljebreen on 21/12/1443 AH.
+//
+
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
+
+struct RegisterView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State var isLoginMode = false
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var selectedType: Int = 1
+    @State private var showingAlert: Bool = false
+    @State private var alertTitle: String = ""
+    @State private var showingLoginScreen = false
+
+  @State private var FullName = ""
+//    @State private var Email = ""
+  @State private var PhoneNumber = ""
+
+  @State private var wrongFullName: Float = 0
+//    @State private var wrongEmail: Float = 0
+
+//    @State var shouldGoToWhatEverPage2: Bool = false
+
+    func registerAction(user: String, pass: String){
+        
+        Auth.auth().createUser(withEmail: user, password: pass) { authResult, error in
+            if let error = error {
+                showAlert("\(error.localizedDescription)")
+            }else if let result = authResult {
+                let uid = result.user.uid
+                //let email = result.user.email
+                //print("\(uid) ,,, \(email)")
+                setUserTypeAction(uid: uid)
+            }else{
+                showAlert("Something went wrong, please try agin.")
+            }
+        }
+    }
+
+    func setUserTypeAction(uid: String){
+        
+        let db = Firestore.firestore()
+        //var ref: DocumentReference? = nil
+        
+        let userData : [String:Any] = [
+            "uid": uid,
+            "type": selectedType,
+            "timestamp": Date().timeIntervalSince1970
+        ]
+        
+        db.collection("users").document(uid).setData(userData) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+                //go back to login
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+
+//        ref = db.collection("users").addDocument(data: userData) { err in
+//            if let err = err {
+//                showAlert("Error adding document: \(err)")
+//            } else {
+//                showAlert("Document added with ID: \(ref?.documentID ?? "N/A")")
+//            }
+//        }
+        
+    }
+    
+    func showAlert(_ title: String){
+        alertTitle = title
+        showingAlert = true
+    }
+//  التصميم
+    var body: some View {
+        NavigationView {
+            ScrollView {
+            
+                VStack(spacing: 10) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Client Account")
+                            .tag(true)
+                        Text("Driver Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .background(.mint)
+
+
+                        Button {
+
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.mint)
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    Group {
+                        TextField("Full Name", text: $FullName)
+                                       
+                     .border(.red, width: CGFloat(wrongFullName))
+                                       
+                                    
+                    TextField("Phone Number", text: $PhoneNumber)
+        TextField(
+                    "User name (email address)",
+                    text: $username)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                
+        SecureField(
+                    "Password",
+                    text: $password
+                )
+                    }.padding()
+                    
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    
+                    if !isLoginMode {
+                        Text("Kindly provide your driving license")
+                        .font(.system(size: 18, weight: .semibold, design: .serif))
+                       ZStack{
+                           image1()
+                        }
+                        
+                        Text("Kindly provide your car insurance")
+                            .font(.system(size: 18, weight: .semibold, design: .serif))
+                      ZStack{
+                            image1()
+
+                      }
+                    }
+                    Button {
+                        handleAction()
+                        registerAction(user: username, pass: password)
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Create Client Account" : "Create Driver Account")
+                                .foregroundColor(.white)
+                                .font(.system(size: 18, weight: .semibold, design: .serif))
+
+                                   .frame(width: 300, height: 50)
+                                   .background(Color(UIColor.systemMint))
+                                    .cornerRadius(10)
+//                    registerAction(user: username, pass: password)
+                            
+                            Spacer()
+            }
+                    }
+                
+            .padding()
+            
+          
+               
+        }
+            }
+        .navigationTitle("New Registration")
+        .background(Color(.init(gray: 0, alpha: 0.05))
+                        .ignoresSafeArea())
+        .alert(alertTitle, isPresented: $showingAlert) {
+            Button("OK", role: .cancel) { }
+        }
+        }
+    }
+        private func handleAction() {
+            if isLoginMode {
+                print("Should log into Firebase with existing credentials")
+            } else {
+                print("Register a new account inside of Firebase Auth and then store image in Storage somehow....")
+            }
+        }
+
+}
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
--- porgct 1/loginView.swift
+++ porgct 1/loginView.swift
@@ -6,107 +6,177 @@
 //
 
 import SwiftUI
-import Foundation
-import Firebase
 import FirebaseAuth
+import FirebaseFirestore
 
 struct LoginView: View {
     
-      @State private var Email = ""
-      @State private var password = ""
-      @State private var wrongEmail: Float = 0
-      @State private var wrongPassword: Float  = 0
-      @State private var showingLoginScreen = false
-   @State var shouldGoToWhatEverPage3: Bool = false
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var showingAlert: Bool = false
+    @State private var alertTitle: String = ""
+    @State private var presentClient: Bool = false
+    @State private var presentProvider: Bool = false
+
+    func loginAction(user: String, pass: String){
+        
+        Auth.auth().signIn(withEmail: user, password: pass) { authResult, error in
+
+            if let error = error {
+                showAlert("\(error.localizedDescription)")
+            }else if let uid = authResult?.user.uid {
+                getUserType(uid: uid)
+            }else{
+                showAlert("Something went wrong, please try agin.")
+            }
+        }
+    }
+    
+    func getUserType(uid: String){
+        
+        let db = Firestore.firestore()
+        
+        
+        db.collection("users").document(uid).getDocument() { (document, err) in
+            
+            if let err = err {
+                showAlert("Error getting documents: \(err)")
+                return
+            }
+            
+            guard let document = document, document.exists,
+                  let data = document.data() else {
+                return
+            }
+            
+            if let type = data["type"] as? Int {
+                
+                //save user type to use it later in the app
+                UserDefaults.standard.set(type, forKey: "currentUserType")
+                
+                //check user type
+                switch type {
+                case 1:
+                    //go to client views
+                    presentClient = true
+                case 2:
+                    //go to provider views
+                    presentProvider = true
+                default:
+                    showAlert("User type is not supported")
+                }
+                
+                
+            }else{
+                //rare case
+                showAlert("Could not find user type, let the user select the type before going to the app")
+            }
+        }
+        
+//        db.collection("users").whereField("uid", isEqualTo: uid).limit(to: 1).getDocuments() { (querySnapshot, err) in
+//            if let err = err {
+//                showAlert("Error getting documents: \(err)")
+//            } else if let query = querySnapshot, let document = query.documents.first {
+//
+//                let data = document.data()
+//                if let type = data["type"] as? Int {
+//                    UserDefaults.standard.set(type, forKey: "currentUserType")
+//                    if type == 1 {
+//                        //go to client views
+//                        presentClient = true
+//                    }else if type == 2 {
+//                        //go to client views
+//                        presentProvider = true
+//                    }else{
+//                        showAlert("User type is not supported")
+//                    }
+//                }else{
+//                    showAlert("Could not find user type, let the user select the type before going to the app")
+//                }
+//            }else{
+//                showAlert("Something went wrong, please try agin.")
+//            }
+//        }
+        
+    }
+    
+    func showAlert(_ title: String){
+        alertTitle = title
+        showingAlert = true
+    }
     
     var body: some View {
-        ZStack{
+        NavigationView{
             ZStack{
-            Color(.systemMint)
-                .ignoresSafeArea()
-            }
-        
-            ZStack{
-            Rectangle()
-                .frame(width: 380, height: 690)
-                .foregroundColor(Color("Color1"))
-                .cornerRadius(20)
-                .offset(y:58)
                 ZStack{
-                    VStack{
-                           Text("Login")
-                            .font(.system(size: 18, weight: .semibold, design: .serif))
-                              
-
-               TextField("Email", text: $Email)
-                          .padding()
-                           .frame(width: 300, height: 50)
-                           .background(Color.white)
-                         .cornerRadius(10)
-                 .border(.red, width: CGFloat(wrongEmail))
-//                 .offset( y:-70 )
-
-
-           SecureField("Password", text: $password)
-                            .padding()
-                             .frame(width: 300, height: 50)
-                              .background(Color.white)
-                             .cornerRadius(10)
-                      .border(.red, width: CGFloat(wrongPassword))
-//                              .offset( y:-70 )
-
-//                         HStack{
-//                            Button{} label: {
-//                              Text("Forgot password?")
-//                                 .underline()
-//                                      .font(.footnote)
-//                                      .bold()
-//                                     .foregroundColor(.black)
-//                                    .padding(.top)
-//                         offset(x: -10, y: -10)
-//                      }
-//                       }
-       
-    
-                        NavigationLink(isActive: $shouldGoToWhatEverPage3, destination: {
-                            new_order_sarah2()
-                        }, label: {
-                            
-                        })
-                        
-                          Button("Login") {
-                              shouldGoToWhatEverPage3.toggle()
-
-                 login(email: Email, password: password)
-                                              }
-                          .foregroundColor(.white)
-                          .font(.system(size: 18, weight: .semibold, design: .serif))
-                          .frame(width: 300, height: 50)
-                           .background(Color(UIColor.systemMint))
-                          .cornerRadius(10)
-                       .offset(y:25 )
-                        
-                    }
+                Color(.systemMint)
+                    .ignoresSafeArea()
+                }
+                ZStack{
+                Rectangle()
+                    .frame(width: 380, height: 690)
+                    .foregroundColor(Color("Color1"))
+                    .cornerRadius(20)
+                 
+                    ZStack{
+                        VStack(alignment: .center, spacing: 50) {
+                VStack {
+        TextField(
+                        "User name (email address)",
+                        text: $username)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                   
+                     .frame(width: 300, height: 50)
+                      .background(Color.white)
+                     .cornerRadius(10)
+        SecureField(
+                        "Password",
+                        text: $password
+                    )
+                  
+                     .frame(width: 300, height: 50)
+                      .background(Color.white)
+                     .cornerRadius(10)
                     
-                          }
+                    Button("Login") {
+                        loginAction(user: username, pass: password)
+                    }
+                    .foregroundColor(.white)
+                    .font(.system(size: 18, weight: .semibold, design: .serif))
+                    .frame(width: 300, height: 50)
+                     .background(Color(UIColor.systemMint))
+                    .cornerRadius(10)
                 
-                       }
                 }
+                .padding()
+                
+                HStack{
+                    Text("You don't have account ?")
+                    NavigationLink{
+                        RegisterView()
+                    } label: {
+                        Text("Register")
+                    }
                 }
+                
             }
-    
-func login(email: String,password: String) {
-               Auth.auth().signIn(withEmail: email, password: password) { res, error in
-                   if error == nil {
-                       print("Succeeded")
-                       
-                   } else {
-                       
-                       print(error?.localizedDescription)
-                   }
-               }
-           }
-
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK", role: .cancel) { }
+            }
+//            .fullScreenCover(isPresented: $presentClient, content: { ClientView() })
+//            .fullScreenCover(isPresented: $presentProvider, content: { ProviderView() })
+            .navigationTitle("Login")
+           
+        }
+     .navigationViewStyle(StackNavigationViewStyle())
+        
+        }
+    }
+//    end Z
+    }
+}
+}
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
